3a2d414df16c2f7a4006c44d0a6957119f682430
Rohit Tanwani <127624104+rohit-tanwani@users.noreply.github.com>
2026-08-08 00:56:38 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> mapOfNumberVsFrequency = new HashMap<Integer, Integer>();
+        for(Integer num: nums) {
+            if(!mapOfNumberVsFrequency.containsKey(num)) {
+                mapOfNumberVsFrequency.put(num, 0);
+            }
+            mapOfNumberVsFrequency.put(num, mapOfNumberVsFrequency.get(num) + 1);
+        }
+
+        TreeMap<Integer, List<Integer>> mapOfFreqVsNumbers = new TreeMap<Integer, List<Integer>>(Collections.reverseOrder());
+        for(Integer num: mapOfNumberVsFrequency.keySet()) {
+            Integer freq = mapOfNumberVsFrequency.get(num);
+            if(!mapOfFreqVsNumbers.containsKey(freq)) {
+                mapOfFreqVsNumbers.put(freq, new ArrayList<Integer>());
+            }
+            mapOfFreqVsNumbers.get(freq).add(num);
+        }
+
+        int[] result = new int[k];
+        int i = 0;
+        for(int freq: mapOfFreqVsNumbers.keySet()) {
+
+            List<Integer> numbers = mapOfFreqVsNumbers.get(freq);
+            for(int num: numbers) {
+                if(i < k) {
+                    result[i++] = num;
+                }
+            }
+
+        }
+        
+        return result;
+    }
+}
