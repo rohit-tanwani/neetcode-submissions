b0710c2614f2d26c64b3642f86bb04c06c7d4c63
Rohit Tanwani <127624104+rohit-tanwani@users.noreply.github.com>
2026-08-08 17:58:14 +0530

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        abc: for(int i = 0, j = s.length() - 1; i < s.length() && j >= 0 && i <= j; i++, j--) {
+            char firstChar = s.charAt(i);
+            char lastChar = s.charAt(j);
+
+            while(!checkForCharacter(firstChar)) {
+                i++;
+                if(i >= s.length()) {
+                    break abc;
+                }
+                firstChar = s.charAt(i);
+            }
+
+            while(!checkForCharacter(lastChar)) {
+                j--;
+                if(j < 0) {
+                    break abc;
+                }
+                lastChar = s.charAt(j);
+            }
+
+            if(firstChar != lastChar) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkForCharacter(char ch) {
+        return ((ch >= 'a' && ch <= 'z') || (ch >='0' && ch<='9'));
+    }
+    
+}
