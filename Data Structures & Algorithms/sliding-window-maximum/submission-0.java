165e4e3b9d43c445ef3673c6c5f54d85034484a7
Rohit Tanwani <127624104+rohit-tanwani@users.noreply.github.com>
2026-08-09 23:51:11 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] result = new int[nums.length - k + 1];
+
+        int windowStart = 0;
+        int windowEnd = k;
+        int index = 0;
+
+        TreeMap<Integer, Set<Integer>> mapOfNumberVsIndex = new TreeMap<Integer, Set<Integer>>();
+        for(int i = windowStart; i < windowEnd; i++) {
+            if(!mapOfNumberVsIndex.containsKey(nums[i])) {
+                mapOfNumberVsIndex.put(nums[i], new HashSet<Integer>());
+            } 
+            mapOfNumberVsIndex.get(nums[i]).add(i);
+        }
+
+        result[index] = mapOfNumberVsIndex.lastKey();
+        while(windowEnd < nums.length) {
+
+            int startKey = nums[windowStart];
+            Set<Integer> indices = mapOfNumberVsIndex.get(startKey);
+            if(indices.size() == 1) {
+                mapOfNumberVsIndex.remove(startKey);
+            }
+            else {
+                indices.remove(windowStart);
+            }
+            windowStart++;
+
+            int endKey = nums[windowEnd];
+            if(!mapOfNumberVsIndex.containsKey(endKey)) {
+                mapOfNumberVsIndex.put(endKey, new HashSet<Integer>());
+            }
+            mapOfNumberVsIndex.get(endKey).add(windowEnd);
+            windowEnd++;
+            result[++index] = mapOfNumberVsIndex.lastKey();
+        }
+        return result;
+    }
+}
